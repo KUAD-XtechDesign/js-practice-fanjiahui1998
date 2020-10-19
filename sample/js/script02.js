fd6de7f72d04aac5bdcc5e9a1d71157431230def
fanjiahui1998 <68629718+fanjiahui1998@users.noreply.github.com>
2020-10-19 14:00:55 +0800

    change1

---
+++ sample/js/script02.js
@@ -0,0 +1,39 @@
+$(function(){
+    
+   let txt1 ="zaoshanghao!";
+   let txt2 ="ogennkidesuka?";
+   let num1 =20;
+   let num2= 32;
+
+
+    $("#button01").on("click",function(){
+      $("#content").text("HELLO!");
+
+    })
+
+    $("#button02").on("click",function(){
+      $("#content").text(txt1);
+
+    })
+
+   
+    $("#button03").on("click",function(){
+       $("#content").text(txt1+txt2);
+
+    })
+
+    $("#button04").on("click",function(){
+        $("#content").text("answer"+num1+num2);
+ 
+    })
+
+    
+    $("#button05").on("click",function(){
+       $("#content").append(num1+"+"+num2+"="+(num1+num2)+".");
+
+
+    })
+   
+
+
+})
